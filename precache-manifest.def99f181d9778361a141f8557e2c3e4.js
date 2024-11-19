4e75b117b28277b1cbfa49b861438a532ca732a2
Prashantkaushik07 <160236438+Prashantkaushik07@users.noreply.github.com>
2024-11-19 18:15:02 +0530

    Updates

---
+++ precache-manifest.def99f181d9778361a141f8557e2c3e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dfc83663866c17ec7d08a3d782a209d",
+    "url": "/TextUtilis/index.html"
+  },
+  {
+    "revision": "5497285f59d9573440f2",
+    "url": "/TextUtilis/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "e5e19857f6fd81909033",
+    "url": "/TextUtilis/static/css/main.f77632a9.chunk.css"
+  },
+  {
+    "revision": "5497285f59d9573440f2",
+    "url": "/TextUtilis/static/js/2.29af3025.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/TextUtilis/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "e5e19857f6fd81909033",
+    "url": "/TextUtilis/static/js/main.6ddb0904.chunk.js"
+  },
+  {
+    "revision": "d1e4fb0d56a0262b27da",
+    "url": "/TextUtilis/static/js/runtime~main.9709bf69.js"
+  }
+]);
