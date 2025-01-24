ab068282d600f84b24c9b50fde365d4ee884faef
Prashant Kaushik <https://Prashantkaushik07.github.io/TextUtilis>
2025-01-24 19:15:21 +0530

    Updates

---
+++ precache-manifest.12bca823b5cb79cf1d1ca793eb5d7e19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f672abe5184fad33bba5c3a821067e70",
+    "url": "/TextUtilis/index.html"
+  },
+  {
+    "revision": "5497285f59d9573440f2",
+    "url": "/TextUtilis/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "68fec7aa9608ef711f87",
+    "url": "/TextUtilis/static/css/main.f77632a9.chunk.css"
+  },
+  {
+    "revision": "5497285f59d9573440f2",
+    "url": "/TextUtilis/static/js/2.29af3025.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/TextUtilis/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "68fec7aa9608ef711f87",
+    "url": "/TextUtilis/static/js/main.140f7113.chunk.js"
+  },
+  {
+    "revision": "d1e4fb0d56a0262b27da",
+    "url": "/TextUtilis/static/js/runtime~main.9709bf69.js"
+  }
+]);
